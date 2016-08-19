3ba087668915592818c5b7e2eccd04b3f364ef3b
Luke Hodkinson <furious.luke@gmail.com>
2016-08-19 12:39:34 +1000

    Disable the prepublish hook. :(

--- djcelery/__init__.py
+++ djcelery/__init__.py
@@ -39,26 +39,26 @@ from celery.states import PENDING
 from celery import current_app as celery  # noqa
 
 
-if getattr(settings, 'CELERY_TRACK_PUBLISH', False):
-    @before_task_publish.connect
-    def update_sent_state(sender=None, body=None, exchange=None,
-                          routing_key=None, **kwargs):
-        if not getattr(settings, 'CELERY_TRACK_PUBLISH', False):
-            # Check again to support dynamic change of this settings
-            return
-        task = celery.tasks.get(sender)
-        if getattr(task, 'ignore_result', False) or getattr(
-                settings, 'CELERY_IGNORE_RESULT', False):
-            # Do not save this task result
-            return
+# if getattr(settings, 'CELERY_TRACK_PUBLISH', False):
+#     @before_task_publish.connect
+#     def update_sent_state(sender=None, body=None, exchange=None,
+#                           routing_key=None, **kwargs):
+#         if not getattr(settings, 'CELERY_TRACK_PUBLISH', False):
+#             # Check again to support dynamic change of this settings
+#             return
+#         task = celery.tasks.get(sender)
+#         if getattr(task, 'ignore_result', False) or getattr(
+#                 settings, 'CELERY_IGNORE_RESULT', False):
+#             # Do not save this task result
+#             return
 
-        backend = task.backend if task else celery.backend
-        request = Context()
-        request.update(**body)
-        request.date_submitted = now()
-        request.delivery_info = {
-            "exchange": exchange,
-            "routing_key": routing_key
-        }
-        backend.store_result(body["id"], None, PENDING, traceback=None,
-                             request=request, body=body)
+#         backend = task.backend if task else celery.backend
+#         request = Context()
+#         request.update(**body)
+#         request.date_submitted = now()
+#         request.delivery_info = {
+#             "exchange": exchange,
+#             "routing_key": routing_key
+#         }
+#         backend.store_result(body["id"], None, PENDING, traceback=None,
+#                              request=request, body=body)
